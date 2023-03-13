ad7ffbd47cb8ae5a8a8dbba2691b26612809bc4f
hoangvu563 <119787028+hoangvu563@users.noreply.github.com>
2023-03-13 23:25:45 +0700

    mini

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>() { 0, 2, 3, 5, 4, 8, 9 };
+            HashSet<int> set2 = new HashSet<int>() { 20, 2,23, 55, 44, 8, 9 };
+            set1.IntersectWith(set2);
+            foreach (var item in set1)
+            { Console.WriteLine(item);}
+        }
+    }
+}
